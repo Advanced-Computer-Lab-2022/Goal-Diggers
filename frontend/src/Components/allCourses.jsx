33fe3b6fabc16402e6428e7a53d2eaf3f2415ac5
Mahmoud Sayed <68595795+MahmoudSayed129@users.noreply.github.com>
2022-10-31 12:04:47 +0200

    Delete frontend directory

--- frontend/src/Components/allCourses.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AllCourses = () => {
-    return (  
-        <React.Fragment></React.Fragment>
-    );
-}
- 
-export default AllCourses;
--- frontend/src/Components/common/Form.jsx
+++
@@ -1,8 +0,0 @@
-import React, {useState} from "react";
-const Form = () => {
-    const [data, setDate] = useState({});
-    const [errors, setErrors] = useState({});
-    return (  <h1>d</h1>);
-}
- 
-export default Form;
